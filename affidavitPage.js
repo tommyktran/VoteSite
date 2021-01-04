9067851b300738562c0e454ad932fea34e5e2a2f
Kevin Le <kevinle108@gmail.com>
2021-01-04 14:27:49 -0500

    changed inpt to password

--- affidavitPage.js
+++ affidavitPage.js
@@ -1,3 +1,5 @@
 function submit() {
-    document.documentElement.innerHTML = '<h1 style="text-align:center;font-family:arial;">Thank you for voting!</h1>';
+    if (document.getElementById('signedby').value === 'vote!') {
+        document.documentElement.innerHTML = '<h1 style="text-align:center;font-family:arial;">Thank you for voting!</h1>';
+    }
 }
