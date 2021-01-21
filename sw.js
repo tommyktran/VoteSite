f85820b52501aeaf289e05f7ff5c18426969ea5d
kevinle108 <kevinle108@gmail.com>
2021-01-21 11:02:08 -0500

    add caching for offline functionality

--- sw.js
+++ sw.js
@@ -1,14 +1,33 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    'affidavitPage.js',
+    '/ballot.css',
+    '/ballot.js',
+    '/buildReviewPage.js',
+    '/buildSelectionPage.js',
+    '/ovalSelection.js',
+    '/WATERVILLE.js'
+];
+
 // install event
 self.addEventListener('install', evt => {
     console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-    console.log('service worker activated');
+    //console.log('service worker activated');
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-    console.log('service worker fetched', evt);
+    //console.log('service worker fetched', evt);
 });
