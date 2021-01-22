d43f46281d7af4788855edc25a89231bce3f280d
Kevin Le <kevinle108@gmail.com>
2021-01-22 09:09:39 -0500

    clean up code

--- affidavitPage.js
+++ affidavitPage.js
@@ -1,5 +1,7 @@
 function submit() {
     if (document.getElementById('signedby').value === 'vote!') {
-        document.documentElement.innerHTML = '<h1 style="text-align:center;font-family:arial;">Thank you for voting!</h1>';
+        document.documentElement.innerHTML = '<h1 id="thankyouForVoting" style="text-align:center;font-family:arial;">Thank you for voting!</h1>';
+        document.getElementById('thankyouForVoting').scrollIntoView();
+        document.getElementById('thankyouForVoting').focus();
     }
 }
