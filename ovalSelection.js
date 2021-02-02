c69b3829d2e33234546d2baa592fa81a5f94889b
Kevin Le <kevinle108@gmail.com>
2021-02-02 13:49:31 -0500

    modal focus trap

--- ovalSelection.js
+++ ovalSelection.js
@@ -226,6 +226,36 @@ function modalAnswer(ovalId, candidateSelections, rankSelections, answer, savedW
 function showModal() {
     document.getElementById("modal").style = 'display:block;'
     document.getElementById("overlay").style = 'display:block;'
+
+    // add all the elements inside modal which you want to make focusable
+    const  focusableElements = ['button'];
+    const modal = document.querySelector('modal'); // select the modal by it's id
+
+    const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+    const focusableContent = modal.querySelectorAll(focusableElements);
+    const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+
+    document.addEventListener('keydown', function(e) {
+    let isTabPressed = e.key === 'Tab';
+
+    if (!isTabPressed) {
+    return;
+    }
+
+    if (e.shiftKey) { // if shift key pressed for shift + tab combination
+    if (document.activeElement === firstFocusableElement) {
+    lastFocusableElement.focus(); // add focus for the last focusable element
+    e.preventDefault();
+    }
+    } else { // if tab key is pressed
+    if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+    firstFocusableElement.focus(); // add focus for the first focusable element
+    e.preventDefault();
+    }
+    }
+    });
+    firstFocusableElement.focus();
 }
 function hideModal() {
     document.getElementById("modal").style = 'display:none;'
