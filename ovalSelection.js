29ba8a532c1793a2542e0771ff951658a1e5e361
Kevin Le <kevinle108@gmail.com>
2021-02-08 10:35:36 -0500

    added esc keydown to modal

--- ovalSelection.js
+++ ovalSelection.js
@@ -214,20 +214,23 @@ function showModal() {
     const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
 
     document.addEventListener('keydown', function(e) {
-        
         let isTabPressed = e.key === 'Tab';
-        if (!isTabPressed) return;
-        if (e.shiftKey) { // if shift key pressed for shift + tab combination
+        let isEscPressed = e.key === 'Escape';
+        if (!isTabPressed && !isEscPressed) return;
+        if (e.shiftKey && isTabPressed) { // if shift key pressed for shift + tab combination
             if (document.activeElement === firstFocusableElement) {
             lastFocusableElement.focus(); // add focus for the last focusable element
             e.preventDefault();
             }
-        } 
-        else { // if tab key is pressed
+        }
+        else if (isTabPressed && !isEscPressed ) {
             if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
-            firstFocusableElement.focus(); // add focus for the first focusable element
-            e.preventDefault();
+                firstFocusableElement.focus(); // add focus for the first focusable element
+                e.preventDefault();
             }
+        } 
+        else if (isEscPressed && !isTabPressed) {
+            hideModal();
         }
     });
     firstFocusableElement.focus();
